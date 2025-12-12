7815acc261b299bf142f18b52e1890e7d3ea62e8
Oleksiy_Lishchuk <ejik.alex9294@gmail.com>
2025-12-13 01:51:31 +0200

    08-zustand v0.9.8

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,19 +1,14 @@
 // app/notes/[id]/page.tsx
-"use client";
-
 import NoteDetails from "@/components/NoteDetails/NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 import type { Note } from "@/types/note";
 
-interface NotePageProps {
-  params: {
-    id: string;
-  };
-}
+// Не типізуємо params вручну, щоб уникнути конфлікту з Next.js PageProps
+export default async function NotePage({ params }: any) {
+  const { id } = params;
 
-export default async function NotePage({ params }: NotePageProps) {
   try {
-    const note: Note = await fetchNoteById(params.id);
+    const note: Note = await fetchNoteById(id);
 
     return <NoteDetails note={note} />;
   } catch (error) {
