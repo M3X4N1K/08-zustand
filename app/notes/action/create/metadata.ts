99c11a8a3cbd98fbd7ea0e2b4498a4cfaa32188a
Oleksiy_Lishchuk <ejik.alex9294@gmail.com>
2025-12-13 00:59:55 +0200

    08-zustand v0.8

---
+++ app/notes/action/create/metadata.ts
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Create Note - NoteHub",
+  description: "Create a new note in NoteHub application.",
+  openGraph: {
+    title: "Create Note - NoteHub",
+    description: "Create a new note in NoteHub application.",
+    url: "https://your-vercel-domain.vercel.app/notes/action/create",
+    images: ["https://ac.goit.global/fullstack/react/notehub-og-meta.jpg"],
+  },
+};
--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -4,17 +4,6 @@ import React from "react";
 import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./page.module.css";
 
-export const metadata = {
-  title: "Create Note - NoteHub",
-  description: "Create a new note in NoteHub application.",
-  openGraph: {
-    title: "Create Note - NoteHub",
-    description: "Create a new note in NoteHub application.",
-    url: "https://your-vercel-domain.vercel.app/notes/action/create",
-    images: ["https://ac.goit.global/fullstack/react/notehub-og-meta.jpg"],
-  },
-};
-
 const CreateNotePage = () => {
   return (
     <main className={css.main}>
